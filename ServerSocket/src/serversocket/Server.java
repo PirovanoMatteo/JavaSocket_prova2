c1843c859905320acad292cde31367895df0316b
Pirovano Matteo <pirovano.matteo@studenti.salesianisesto.it>
2022-02-20 12:11:15 +0100

    init repo

---
+++ ServerSocket/src/serversocket/Server.java
@@ -0,0 +1,46 @@
+/* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serversocket;
+
+import serversocket.Server.ServerSocket;
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pogliani.mattia
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            boolean fine = false;
+            String messaggio;
+            while (!fine) {
+                messaggio = in.readLine();
+                if (messaggio.equals("fine")) {
+                    fine = true;
+                }
+                out.println(messaggio.toUpperCase());
+            }            
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
